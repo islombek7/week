d3ace181a4a45dd55eaefc0ac7211e88435c51fe
islombek <sirojr91@gmail.com>
2022-10-11 17:42:08 +0500

    WEEK

---
+++ jc/script.js
@@ -0,0 +1,25 @@
+let week = prompt(' HAFTA KUNINI YOZING ')
+switch (week)
+{
+     case 'MONDAY':
+        console.log('TUESDAY');
+        break;
+     case 'TUESDAY':
+        console.log('WEDNESDAY')
+        break;
+    case 'WEDNESDAY':
+        console.log('THURSDAY');
+        break;
+    case 'THURSDAY':
+        console.log('FRIDAY');
+        break;
+    case 'FRIDAY':
+        console.log('SATURDAY');
+        break;
+    case 'SATURDAY':
+        console.log('SUNDAY');
+        break;
+    case 'SUNDAY':
+        console.log('MONDAY');
+        break;
+}
